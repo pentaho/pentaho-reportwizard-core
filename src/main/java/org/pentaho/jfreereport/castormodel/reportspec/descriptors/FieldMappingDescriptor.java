3dd98abfaf6f74b5325bda7aa72d38b944191f94
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:23:48 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit d8d71abe54ca1a27d7c27286675d331e5812eed6.

--- src/main/java/org/pentaho/jfreereport/castormodel/reportspec/descriptors/FieldMappingDescriptor.java
+++ src/main/java/org/pentaho/jfreereport/castormodel/reportspec/descriptors/FieldMappingDescriptor.java
@@ -1,14 +1,3 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
 /*
  * This class was automatically generated with 
  * <a href="http://www.castor.org">Castor 1.4.1</a>, using an XML
