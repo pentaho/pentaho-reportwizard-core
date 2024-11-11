c1632a983ff7b4a376a0e8e0e9023ad0ef0b6a72
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:40:17 -0500

    DEVO-11194: updated license and copyright headers

--- src/main/java/org/pentaho/jfreereport/castormodel/reportspec/Series.java
+++ src/main/java/org/pentaho/jfreereport/castormodel/reportspec/Series.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2029-07-20
+ ******************************************************************************/
 /*
  * This class was automatically generated with 
  * <a href="http://www.castor.org">Castor 1.4.1</a>, using an XML
